51196b9cb74192093b126ba55b35003d5c82cdfc
daneporter <dane@ropedigital.com.au>
2019-10-07 15:35:36 +1100

    Update ropedigital-manager.php

--- ropedigital-manager.php
+++ ropedigital-manager.php
@@ -50,11 +50,3 @@ function dt_list_table_views($views){
    return $views;
 }
 
-/*** Remove Query String from Static Resources ***/
-function remove_cssjs_ver( $src ) {
- if( strpos( $src, '?ver=' ) )
- $src = remove_query_arg( 'ver', $src );
- return $src;
-}
-add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
-add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
