9a47a4add2cad1967d91f9094f0eea3e8bba4ef2
daneporter <dane@ropedigital.com.au>
2019-10-25 16:09:12 +1100

    Create create-admin.sh

---
+++ scripts/create-admin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Create WP Admin User Account
+
+app_name=$1
+usr_name=$2
+usr_email=$3
+usr_pass=$4
+
+
+
+sudo -u $usr_name -i -- wp user create $usr_email $usr_email --user_pass=$usr_pass --role=administrator  --path=/srv/users/$usr_name/apps/$app_name/public/
+#Update Timezone to Melbourne
+sudo -u $usr_name -i -- wp option update timezone_string "Australia/Melbourne" --path=/srv/users/$usr_name/apps/$app_name/public/
+
+echo done
