27f7a20abf20c43eaa95603177a302623fd37be8
daneporter <dane@ropedigital.com.au>
2019-10-25 16:11:05 +1100

    Create env-setup.sh

---
+++ scripts/env-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Setup WP Envrionment after initial setup
+
+app_name=$1
+usr_name=$2
+
+
+cd /srv/users/$usr_name/apps/$app_name/public/wp-content/
+mkdir mu-plugins
+cd mu-plugins
+wget https://raw.githubusercontent.com/daneporter/ropedigital/master/ropedigital-manager.php
+
+cd /srv/users/$usr_name/apps/$app_name/public/
+wget https://raw.githubusercontent.com/daneporter/ropedigital/master/.htaccess
+
+
+echo done
+
