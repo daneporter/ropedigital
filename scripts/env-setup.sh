40af2c9971234ef069f52b26a9ad8af72b8e6387
daneporter <dane@ropedigital.com.au>
2019-10-26 13:57:49 +1100

    Update env-setup.sh

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -4,12 +4,13 @@
 app_name=$1
 usr_name=$2
 
-
+# Move to target WP directory and create mu-plugins directoy and download management plugin
 cd /srv/users/$usr_name/apps/$app_name/public/wp-content/
 mkdir mu-plugins
 cd mu-plugins
 wget https://raw.githubusercontent.com/daneporter/ropedigital/master/ropedigital-manager.php
 
+# Download standard htaccess file - Contains appropriate permalink config, browser caching and basic config for Apache hosts
 cd /srv/users/$usr_name/apps/$app_name/public/
 wget https://raw.githubusercontent.com/daneporter/ropedigital/master/.htaccess
 
