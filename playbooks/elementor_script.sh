239fc358af19c9b616e335468b2e1725c700e8c0
daneporter <dane@ropedigital.com.au>
2019-11-20 11:19:34 +1100

    Update elementor_script.sh

--- playbooks/elementor_script.sh
+++ playbooks/elementor_script.sh
@@ -8,9 +8,9 @@ usr_name=$2
 
 
 sudo -u $usr_name -i -- wp plugin delete akismet hello --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp theme delete twentyseventeen twentysixteen --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp post delete $(wp post list --post_type='post' --format=ids --path=/srv/users/$usr_name/apps/$app_name/public/ --allow-root) --path=/srv/users/$usr_name/apps/$app_nam$
-sudo -u $usr_name -i -- wp post delete $(wp post list --post_type='page' --format=ids --path=/srv/users/$usr_name/apps/$app_name/public/ --allow-root) --path=/srv/users/$usr_name/apps/$app_nam$
+sudo -u $usr_name -i -- wp theme delete twentynineteen twentyseventeen twentysixteen --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp post delete $(wp post list --post_type='post' --format=ids --path=/srv/users/$usr_name/apps/$app_name/public/ --allow-root) 
+sudo -u $usr_name -i -- wp post delete $(wp post list --post_type='page' --format=ids --path=/srv/users/$usr_name/apps/$app_name/public/ --allow-root)
 sudo -u $usr_name -i -- wp widget delete $(wp widget list sidebar-1 --format=ids) --path=/srv/users/$usr_name/apps/$app_name/public/
 sudo -u $usr_name -i -- wp option update blogdescription "WordPress Hosted by Rope Digital" --path=/srv/users/$usr_name/apps/$app_name/public/
 sudo -u $usr_name -i -- wp option set default_comment_status closed --path=/srv/users/$usr_name/apps/$app_name/public/
@@ -18,9 +18,9 @@ sudo -u $usr_name -i -- wp rewrite structure '/%postname%/' --hard --path=/srv/u
 sudo -u $usr_name -i -- wp plugin install elementor --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 sudo -u $usr_name -i -- wp plugin install "https://github.com/daneporter/ropedigital/raw/master/plugins/elementor-pro-2.7.2.zip" --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 sudo -u $usr_name -i -- wp option update timezone_string "Australia/Melbourne" --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp theme install astra --activate --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp theme install hello-elementor --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 #sudo -u $usr_name -i -- wp plugin install "https://ropedigital.com.au/plugins/astra-addon.zip" --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 #sudo -u $usr_name -i -- wp plugin install "https://ropedigital.com.au/plugins/powerpack-elements.zip" --activate --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp plugin install astra-sites --activate --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp plugin install astra-sites --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 
 echo done
