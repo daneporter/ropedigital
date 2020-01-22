e88270a1882208cea89f9da8a867d42c5ce4f23e
daneporter <dane@ropedigital.com.au>
2020-01-22 14:03:55 +1100

    Update run_playbook.sh

--- scripts/run_playbook.sh
+++ scripts/run_playbook.sh
@@ -16,18 +16,19 @@ app_name=$4
 usr_name=$5
 email=$6
 usr_pw=$7
-playbook=$8
+template_id=$8
+
 
 #Get copies of the latest scripts onto the remote host
 
 sshpass -p $password ssh $username@$ip <<'ENDSSH'
 cd /var/opt/
-wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/playbooks/elementor_script.sh -O elementor_script.sh
-chmod 700 elementor_script.sh
+wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/playbooks/starter_site_playbook.sh -O starter_site_playbook.sh
+chmod 700 starter_site_playbook.sh
 ENDSSH
 
 #Invoke Playbook Setup script on remote host
-sshpass -p $password ssh $username@$ip  ". /var/opt/elementor_script.sh $app_name $usr_name" <<'ENDSSH'
+sshpass -p $password ssh $username@$ip  ". /var/opt/starter_site_playbook.sh $app_name $usr_name $template_id" <<'ENDSSH'
 exit
 ENDSSH
 exit
