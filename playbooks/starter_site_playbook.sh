57555b4e565050c1e12ddc349732c1af3d3f227f
daneporter <dane@ropedigital.com.au>
2020-01-23 14:48:58 +1100

    Update starter_site_playbook.sh

--- playbooks/starter_site_playbook.sh
+++ playbooks/starter_site_playbook.sh
@@ -35,9 +35,9 @@ sudo -u $usr_name -i -- wp plugin install "https://github.com/daneporter/ropedig
 sudo -u $usr_name -i -- wp plugin install "https://github.com/daneporter/ropedigital/raw/master/starter-sites/plugins/astra-premium-sites.zip" --activate --path=/srv/users/$usr_name/apps/$app_name/public/
 
 #Activate Licence for Pro Product Sets
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon f29d834f5cad15f2845fdc6bf9469875 --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 8c6c089af61dac7dcde1fa27da0fe350 --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate uael eafc56423344b436e641215255dff53a --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
 
 #Configure Astra Pro settings and set Elementor as page builder
 sudo -u $usr_name -i -- wp astra-sites page_builder set elementor --path=/srv/users/$usr_name/apps/$app_name/public/
