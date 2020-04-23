e35bdebcd2383684b74a0da592b8593ae17f6b78
daneporter <dane@ropedigital.com.au>
2020-04-23 17:56:44 +1000

    Update starter_site_playbook.sh

--- playbooks/starter_site_playbook.sh
+++ playbooks/starter_site_playbook.sh
@@ -65,10 +65,14 @@ sudo -u $usr_name -i -- wp config set WP_ASTRA_WHITE_LABEL 'true' --path=/srv/us
 #Site ID's can be referenced at https://websitedemos.net/wp-json/wp/v2/astra-sites/
 sudo -u $usr_name -i -- wp astra-sites import $template_id --yes --path=/srv/users/$usr_name/apps/$app_name/public/
 
-
-
 #Exit Maintenance Mode
 sudo -u $usr_name -i -- wp maintenance-mode deactivate --path=/srv/users/$usr_name/apps/$app_name/public/
+
+
+#Activate Licence for Pro Product Sets - Some reason this has to happen twice?
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
 
 #Finish
 echo done
