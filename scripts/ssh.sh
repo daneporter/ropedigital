d9903c14d08cfc21a8cc5c94432605d1ac664d1a
daneporter <dane@ropedigital.com.au>
2020-05-04 16:08:32 +1000

    Update ssh.sh

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -5,26 +5,46 @@
 
 ## Usage: ./ssh.sh [target ssh username] [target ssh password] [target ssh IP] [sp app name] [sp username] [client email] [wp password]
 
+################################################################
+##Change Log##
+## v0.1 - Initial - Cloned from run_playbook.sh
+## v0.2 - Changed from using SSHpass to standard SSH after installing keys
+################################################################
 
 #SSH Remote Script
 
 
+#username=$1
+#password=$2
+#ip=$3
+#app_name=$4
+#usr_name=$5
+#email=$6
+#usr_pw=$7
+
+#V2 - No password
 username=$1
-password=$2
-ip=$3
-app_name=$4
-usr_name=$5
-email=$6
-usr_pw=$7
+ip=$2
+app_name=$3
+usr_name=$4
+email=$5
+usr_pw=$6
 
 #Get copies of the latest scripts onto the remote host
 
-sshpass -p $password ssh $username@$ip <<'ENDSSH'
-cd /var/opt/
+ssh $username@$ip << EOF
+cd /var/opt/ 
 wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/scripts/env-setup.sh -O env-setup.sh
 wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/scripts/create-admin.sh -O create-admin.sh
 chmod 700 *.sh
-ENDSSH
+EOF
+
+#sshpass -p $password ssh $username@$ip <<'ENDSSH'
+#cd /var/opt/
+#wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/scripts/env-setup.sh -O env-setup.sh
+#wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/scripts/create-admin.sh -O create-admin.sh
+#chmod 700 *.sh
+#ENDSSH
 
 #Invoke Create Admin script on remote host
 #sshpass -p $password ssh $username@$ip ". /var/opt/create-admin.sh $app_name $usr_name $email $usr_pw" <<'ENDSSH'
@@ -37,9 +57,14 @@ cd /var/opt/
 ./create-admin.sh $app_name $usr_name $email $usr_pw
 EOF
 
+ssh $username@$ip << EOF
+cd /var/opt/
+./env-setup.sh $app_name $usr_name
+EOF
+
 #Invoke Environment Setup script on remote host
-sshpass -p $password ssh $username@$ip  ". /var/opt/env-setup.sh $app_name $usr_name" <<'ENDSSH'
-exit
-ENDSSH
+#sshpass -p $password ssh $username@$ip  ". /var/opt/env-setup.sh $app_name $usr_name" <<'ENDSSH'
+#exit
+#ENDSSH
 exit
 exit
