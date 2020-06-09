1d607751426bc2a1db54a850bb574b6cf1fd6cc4
daneporter <dane@ropedigital.com.au>
2020-06-09 13:42:21 +1000

    Update starter_site_playbook.sh

--- playbooks/starter_site_playbook.sh
+++ playbooks/starter_site_playbook.sh
@@ -8,8 +8,8 @@ usr_email=$4
 sub_domain=$5
 
 #Update URL to https:
-sudo -u $usr_name -i -- wp option update home "https://$subdomain" --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp option update siteurl "https://$subdomain" --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp option update home $subdomain --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp option update siteurl $subdomain --path=/srv/users/$usr_name/apps/$app_name/public/
 
 
 #Enter Maintenance Mode
