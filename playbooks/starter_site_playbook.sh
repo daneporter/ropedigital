d7b4b94112ae805c6ee4bcf27f0cef358a37d407
daneporter <dane@ropedigital.com.au>
2020-06-15 18:44:38 +1000

    Update starter_site_playbook.sh

--- playbooks/starter_site_playbook.sh
+++ playbooks/starter_site_playbook.sh
@@ -78,14 +78,14 @@ sudo -u $usr_name -i -- wp config set WP_ASTRA_WHITE_LABEL 'true' --path=/srv/us
 sudo -u $usr_name -i -- wp astra-sites import $template_id --yes --path=/srv/users/$usr_name/apps/$app_name/public/
 
 #Exit Maintenance Mode
-sudo -u $usr_name -i -- wp maintenance-mode deactivate --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp maintenance-mode deactivate --path=/srv/users/$usr_name/apps/$app_name/public/
 
 
 #Activate Licence for Pro Product Sets - Some reason this has to happen twice?
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
+#sudo -u $usr_name -i -- wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f --path=/srv/users/$usr_name/apps/$app_name/public/
 
 
 #Update Admin Email
