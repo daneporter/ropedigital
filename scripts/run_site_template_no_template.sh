03aa6ca3becddef472ccc57b56e81f7a0bd0b26d
daneporter <dane@ropedigital.com.au>
2020-08-08 18:18:09 +1000

    Update run_site_template_no_template.sh

--- scripts/run_site_template_no_template.sh
+++ scripts/run_site_template_no_template.sh
@@ -26,8 +26,8 @@ sub_domain=$9
 #Get copies of the latest scripts onto the remote host
 ssh $username@$ip << EOF
 cd /var/opt/ 
-wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/playbooks/starter_site_playbook_no_template.sh -O starter_site_playbook.sh
-chmod 700 starter_site_playbook.sh
+wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/playbooks/starter_site_playbook_no_template.sh -O starter_site_playbook_no_template.sh
+chmod 700 starter_site_playbook_no_template.sh
 EOF
 
 
