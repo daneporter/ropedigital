e918b50019ac57ce2d2b4a0783b9f1fe506bf75b
daneporter <dane@ropedigital.com.au>
2020-08-10 15:51:33 +1000

    Update create-admin.sh

--- scripts/create-admin.sh
+++ scripts/create-admin.sh
@@ -13,7 +13,7 @@ sudo -u $usr_name -i -- wp user create $usr_email $usr_email --user_pass=$usr_pa
 #Update Timezone to Melbourne
 sudo -u $usr_name -i -- wp option update timezone_string "Australia/Melbourne" --path=/srv/users/$usr_name/apps/$app_name/public/
 #Update Site / Blog name
-sudo -u $usr_name -i -- wp option update blogname "$site_name" --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp option update blogname $site_name --path=/srv/users/$usr_name/apps/$app_name/public/
 
 echo done
 exit
