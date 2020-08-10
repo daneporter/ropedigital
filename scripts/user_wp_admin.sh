80afe920fc240275ff083d166c7fd891bb4c428d
daneporter <dane@ropedigital.com.au>
2020-08-10 13:42:12 +1000

    Create user_wp_admin.sh

---
+++ scripts/user_wp_admin.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+## This script needs to exist on the Primary Host/Jumpox/Bastion Server. It is used to tunnel SSH commands to
+## This is used to create a new Admin user, with details provided by the Customer
+## Assumes a hosting instance has been created using serverpilot
+
+## Usage: ./ssh.sh [target ssh username] [target ssh password] [target ssh IP] [sp app name] [sp username] [client email] [wp password]
+
+################################################################
+##Change Log##
+## v0.1 - Initial - Cloned from run_playbook.sh
+## v0.2 - Changed from using SSHpass to standard SSH after installing keys
+################################################################
+
+#SSH Remote Script
+
+#V2 - No password
+
+ip=$1
+app_name=$2
+usr_name=$3
+email=$4
+usr_pw=$5
+
+#Get copies of the latest scripts onto the remote host
+
+ssh $username@$ip << EOF
+cd /var/opt/ 
+wget -q https://raw.githubusercontent.com/daneporter/ropedigital/master/scripts/create-admin.sh -O create-admin.sh
+chmod 700 *.sh
+EOF
+
+
+#Invoke Create Admin script on remote host
+ssh $username@$ip << EOF
+cd /var/opt/ 
+./create-admin.sh $app_name $usr_name $email $usr_pw
+EOF
+
+exit
+exit
