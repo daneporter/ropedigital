969362fb738b945948594d34ebaac6b652107778
daneporter <dane@ropedigital.com.au>
2020-08-14 16:03:03 +1000

    Create maintain_readypress_plugins.sh

---
+++ playbooks/maintain_readypress_plugins.sh
@@ -0,0 +1,7 @@
+
+
+#Activate Licence for Pro Product Sets
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
+sudo -u $usr_name -i -- wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f --path=/srv/users/$usr_name/apps/$app_name/public/
