5af9eb59c37d7c6955fe73d0b22d61f648d2339b
daneporter <dane@ropedigital.com.au>
2020-08-14 16:38:01 +1000

    Update maintain_readypress_plugins.sh

--- playbooks/maintain_readypress_plugins.sh
+++ playbooks/maintain_readypress_plugins.sh
@@ -1,7 +1,12 @@
-
+#!/bin/bash
+# A quick script to update licence when it drops and then update the relevant plugins
 
 #Activate Licence for Pro Product Sets
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb --path=/srv/users/$usr_name/apps/$app_name/public/
-sudo -u $usr_name -i -- wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f --path=/srv/users/$usr_name/apps/$app_name/public/
+wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e
+wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca
+wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb
+wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f
+
+
+
+wp plugin update astra-addon elementor header-footer-elementor elementor-pro astra-pro-sites ultimate-elementor wpforms-lite
