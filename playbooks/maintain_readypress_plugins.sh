8615cd7098241ee2351e47d2c783ae1e2a77c3de
daneporter <dane@ropedigital.com.au>
2020-08-27 10:11:02 +1000

    Update maintain_readypress_plugins.sh

--- playbooks/maintain_readypress_plugins.sh
+++ playbooks/maintain_readypress_plugins.sh
@@ -5,7 +5,7 @@
 wp brainstormforce license activate astra-addon e08971ab3a707c980f0ca371e303523e
 wp brainstormforce license activate astra-pro-sites 44623b583829033d224e16545d8153ca
 wp brainstormforce license activate uael 28d03061107b4b38e7a1d7ce7bc283eb
-wp plugin uninstall elementor-pro
+wp plugin uninstall elementor-pro --deactivate
 wp plugin install "https://github.com/daneporter/ropedigital/raw/master/starter-sites/plugins/elementor-pro.zip" --activate
 wp elementor-pro license activate 39b245eba938b567dbb8cdf095ec447f
 
